486acbb36d7661600c1d10366fe1de91fc8dd0ed
Bichi Kim <bichi@live.co.kr>
2018-06-21 03:08:42 +0900

    update

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,25 +1,63 @@
 import query from './'
 describe('graphql-query-builder', () => {
+  const image = query('image')({size: 50})('url', 'width', 'height')
   it('should work', () => {
-    const image = query('image')({size: 50})('url', 'width', 'height')
     const result = query('user')({name: 'bichi'})(
+      'id',
+      {
+        nickname: 'name',
+        item: ['candy', 'cookie', {
+          ham: ['smoked', {brand: ['spam']}]
+        }],
+        image,
+      },
+    )
+    expect(result.toString()).to.equal(
+      'user(name:"bichi"){' +
+      'id,nickname:"name",item{candy,cookie,ham{smoked,brand{spam}}},' +
+      'image:image(size:50){url,width,height}' +
+      '}'
+    )
+  })
+  it('should work without arguments', () => {
+    const result = query('user')()(
       'id',
       {
         nickname: 'name',
         image,
       },
-    ).toString()
-    expect(result).to.equal(
-      'user(name:bichi){' +
-      'id,nickname:name,' +
+      )
+    expect(result.toString()).to.equal(
+      'user{' +
+      'id,nickname:"name",' +
       'image:image(size:50){url,width,height}' +
       '}'
     )
   })
-  it('should work', () => {
-    const result = query('user')()({name: 'bichi'}).toString()
-    expect(result).to.equal(
-      'user{name:bichi}'
+  it('should work without results', () => {
+    const result = query('user')({name: 'bichi'})().toString()
+    expect(result.toString()).to.equal(
+      'user(name:"bichi")'
     )
+  })
+  it('should work with complex arguments', () => {
+    const result = query('user')({
+      user: {name: 'bob'},
+      friends: [
+        {id: 1, name: 'ann'},
+        {id: 1, name: 'tom'},
+      ],
+    })().toString()
+    expect(result.toString()).to.equal(
+      'user(user:{name:"bob"},friends:[{id:1,name:"ann"},{id:1,name:"tom"}])'
+    )
+  })
+  it('should work without arguments & results', () => {
+    const result = query('user')()()
+    expect(result.toString()).to.equal('user')
+  })
+  it('should work without arguments & results', () => {
+    const result = query('any','my', 'user')({name: 'bichi'})('name', 'age')
+    expect(result.toString()).to.equal('any:my:user(name:"bichi"){name,age}')
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,51 @@ interface IResultObjectItem {
   [name: string]: string
 }
 
-const objectSmoothing = (object: {[name: string]: string | number}): string[] => {
+const dataDeco = (data: any, argumentMode: boolean = true): string => {
+  if(typeof data === 'string'){
+    return `"${data}"`
+  }
+  if(typeof data === 'number'){
+    return `${String(data)}`
+  }
+  if(data instanceof QueryBuilder){
+    return `${data.toString()}`
+  }
+  if(typeof data === 'function'){
+    return dataDeco(data())
+  }
+  if(Array.isArray(data)){
+    if(argumentMode){
+      return `[${data.map((value) => (dataDeco(value))).join(',')}]`
+    }
+    return `{${data.map((value) => {
+      // string won't be changed in the argumentMode
+      return typeof value === 'string' ? value : dataDeco(value, argumentMode)
+    }).join(',')}}`
+  }
+  const result: string = objectSmoothing(data, argumentMode).join(',')
+  if(argumentMode){
+    return `{${result}}`
+  }
+  return `${result}`
+}
+
+const objectSmoothing = (
+  object: {[name: string]: string | number | QueryBuilder},
+  argumentMode: boolean = true
+): string[] => {
   const args: string[] = []
+
   Object.keys(object).forEach((key) => {
-    args.push(`${key}:${object[key]}`)
+    const data = object[key]
+    let separator = ':'
+    if(!argumentMode){
+      const type = typeof data
+      if(type !== 'string' && type !== 'number' && !(data instanceof QueryBuilder)){
+        separator = ''
+      }
+    }
+    args.push(`${key}${separator}${dataDeco(data, argumentMode)}`)
   })
   return args
 }
@@ -22,15 +63,21 @@ const resultFactory = (item: string | IResultObjectItem) => {
   if(typeof item === 'string'){
     return item
   }
-  return objectSmoothing(item).join(',')
+  return objectSmoothing(item, false).join(',')
+}
+
+interface IQueryBuilderPayload {
+  name: string[]
+  args: IArguments
+  results: any[]
 }
 
 class QueryBuilder {
-  private readonly _name: string
+  private readonly _name: string[]
   private readonly _args: IArguments
   private readonly _results: any[]
 
-  constructor(name: string, args: IArguments, results: any[]) {
+  constructor({name, args, results}: IQueryBuilderPayload) {
     this._name = name
     this._args = args
     this._results = results
@@ -38,21 +85,25 @@ class QueryBuilder {
 
   toString() {
     const {_args, _results} = this
-    let _string = this._name
+    let _string = this._name.join(':')
     if(_args){
       _string += `(${objectSmoothing(_args).join(',')})`
     }
-    if(_results){
+    if(_results && _results.length > 0){
       _string += `{${_results.map(resultFactory).join(',')}}`
     }
     return _string
   }
 }
 
-export default (name: string) => {
-  return (args: IArguments) => {
+export default (...name: string[]) => {
+  return (args: any) => {
     return (...results: any[]) => {
-      return new QueryBuilder(name, args, results)
+      return new QueryBuilder({
+        name,
+        results,
+        args,
+      })
     }
   }
 }
